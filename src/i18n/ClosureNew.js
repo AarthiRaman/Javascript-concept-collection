6c0049bda0c7a5a9f2c40bad477264da7c443ab1
AarthiRaman <aarthi.pradha@gmail.com>
2019-07-10 12:18:55 +0530

    Experimenting with new markup type

--- src/i18n/ClosureNew.js
+++ src/i18n/ClosureNew.js
@@ -1,18 +1,32 @@
-export default {
-  title: "Closures in Javascript",
-  definition:
-    "A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment).",
-  placesUsed: ["Data Privacy to emulate private methods", ""],
-  snippet: [
-    `function init() { 
+export default [
+  {
+    type: "heading",
+    text: "Closures in Javascript"
+  },
+  {
+    type: "definition",
+    text:
+      "A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment)."
+  },
+  {
+    type: "text",
+    text: "Data Privacy to emulate private methods"
+  },
+  {
+    type: "snippet",
+    text: `
+    function init() { 
       var name = 'Mozilla'; // name is a local variable created by init function 
       displayName() { // displayName() is the inner function, a closure 
          alert(name); // use variable declared in the parent function 
       } 
       displayName(); 
     } 
-    init();`,
-    `
+    init();`
+  },
+  {
+    type: "snippet",
+    text: `
     var counter = (function() { 
       var privateCounter = 0; 
       function changeBy(val) { 
@@ -36,16 +50,20 @@ export default {
         counter.decrement();
         console.log(counter.value()); // logs 1
     `
-  ],
-  referenceLinks: [
-    {
-      href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures",
-      text: "Closures - JavaScript | MDN "
-    },
-    {
-      text: "Master the JavaScript Interview: What is a Closure",
-      href:
-        "https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-closure-b2f0d2152b36"
-    }
-  ]
-};
+  },
+  {
+    type: "referenceLinks",
+    links: [
+      {
+        href:
+          "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures",
+        text: "Closures - JavaScript | MDN "
+      },
+      {
+        text: "Master the JavaScript Interview: What is a Closure",
+        href:
+          "https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-closure-b2f0d2152b36"
+      }
+    ]
+  }
+];
